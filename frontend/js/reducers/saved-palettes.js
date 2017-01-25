0fa740875cbe38d4f448409714b980d804674276
a <a@b.com>
2017-01-24 23:44:56 -0500

    - Implemented saved palette data reducer cases; Added unit tests for reducer logic

--- frontend/js/reducers/saved-palettes.js
+++ frontend/js/reducers/saved-palettes.js
@@ -1,20 +1,86 @@
 let initialState = {
 	palettes: [],
-	isFetching: false,
+	loading: false,
 	error: false,
 };
 
+let changedPalettes,
+	changedPalette,
+	index;
+
 let savedPalettes = (state = initialState, action) => {
 	switch(action.type) {
+		
+		case 'CREATE':
+		case 'UPDATE':
+		case 'FETCH':
+		case 'DESTROY':
+			return Object.assign({}, state, {
+				loading: true,
+				error: false,
+			});
+
+		case 'FETCH_SUCCESS':
 		case 'CREATE_SUCCESS':
 			return Object.assign({}, state, {
 				palettes: [
 					...state.palettes,
 					action.newPalette,
 				],
+				loading: false,
+				error: false,
 			});
+			
+		case 'UPDATE_SUCCESS':
+			changedPalettes = [...state.palettes];
+			
+			index = changedPalettes.findIndex(
+				palette => palette.id === action.palette.id
+			);
+			
+			// Clone the changed palette so that there is a new
+			// copy without a reference elsewhere in the code
+			changedPalette = JSON.parse(
+				JSON.stringify(
+					action.palette
+				)
+			);
+			
+			changedPalettes.splice(index, 1, changedPalette);
+			
+			return Object.assign({}, state, {
+				palettes: changedPalettes,
+				loading: false,
+				error: false,
+			});
+		
+		case 'DESTROY_SUCCESS':
+			changedPalettes = [...state.palettes];
+
+			index = changedPalettes.findIndex(
+				palette => palette.id === action.id
+			);
+
+			changedPalettes.splice(index, 1);
+
+			return Object.assign({}, state, {
+				palettes: changedPalettes,
+				loading: false,
+				error: false,
+			});
+		
+		case 'CREATE_FAILURE':
+		case 'UPDATE_FAILURE':
+		case 'FETCH_FAILURE':
+		case 'DESTROY_FAILURE':
+			return Object.assign({}, state, {
+				loading: false,
+				error: true,
+			});
+		
 		default:
 			return state;
+			
 	}
 };
 
--- frontend/js/test/saved-palettes.test.js
+++ frontend/js/test/saved-palettes.test.js
@@ -7,11 +7,15 @@ describe('Saved Palettes Reducer', function() {
 
 		assert.deepEqual(state, {
 			palettes: [],
-			isFetching: false,
+			loading: false,
 			error: false,
 		});
 	});
 	
+	// The same action is triggered reducer is triggered for both
+	// Fetch and Create; in the case of Fetch, the palettes array
+	// is empty, so you're just coming an empty array with its new
+	// contents
 	it('should store data when a palette is saved', function() {
 		let newPalette = {
 				_id: '123',
@@ -24,5 +28,72 @@ describe('Saved Palettes Reducer', function() {
 			});
 			
 		assert.deepEqual(newState.palettes[0], newPalette);
+	});
+	
+	it('should update the appropriate data set when updating a particular palette', function() {
+		let firstState = {
+				palettes: [{
+					id: 'test-id',
+					colors: ['color-1'],
+					title: 'test-title',
+				}],
+				loading: true,
+				error: false,
+			},
+			updatedPalette = {
+				id: 'test-id',
+				colors: [ 'color-2', 'color-3' ],
+				title: 'new-title',
+			};
+			
+		let state = reducer(firstState, {
+			type: 'UPDATE_SUCCESS',
+			palette: updatedPalette,
+		});
+
+		assert.deepEqual([updatedPalette], state.palettes);
+		assert.strictEqual(state.loading, false);
+		assert.strictEqual(state.error, false);
+	});
+	
+	it('should remove the deleted palette from the palette store on succesful delete', function() {
+		let firstState = {
+				palettes: [{
+					id: 'test-id',
+					colors: ['color-1'],
+					title: 'test-title',
+				}],
+				loading: true,
+				error: false,
+			};
+			
+		let state = reducer(firstState, {
+			type: 'DESTROY_SUCCESS',
+			id: 'test-id',
+		});
+
+		assert.deepEqual(state.palettes, []);
+		assert.strictEqual(state.loading, false);
+		assert.strictEqual(state.error, false);
+	});
+
+	// Should set data when it is initially fetched
+	// This also tests the cases for 'UPDATE_FAILURE', 'FETCH_FAILURE',
+	// and 'DESTROY_FAILURE'
+	it('should set error on a failed create', function() {
+		let firstState = { palettes: [], loading: true, error: false },
+			newState = reducer(firstState, { type: 'CREATE_FAILURE' });
+		
+			assert.strictEqual(newState.loading, false);
+			assert.strictEqual(newState.error, true);
+	});
+	
+	// This also tests the cases for 'UPDATE', 'FETCH', and 'DESTROY'
+	it('should set loading parameter when initiating a CREATE', function() {
+		let firstState = { palettes: [], loading: true, error: false},
+			newState = reducer(firstState, { type: 'CREATE' });
+		
+		assert.strictEqual(newState.loading, true);
+		assert.strictEqual(newState.error, false);
 	});
 });
